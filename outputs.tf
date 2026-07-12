fddc770474d402956bf29169499549a38a190f5b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "spring_cloud_gateway_route_configs" {
-  description = "All spring_cloud_gateway_route_config resources"
-  value       = azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs
-}
 output "spring_cloud_gateway_route_configs_filters" {
-  description = "List of filters values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.filters]
+  description = "Map of filters values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.filters }
 }
 output "spring_cloud_gateway_route_configs_name" {
-  description = "List of name values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.name]
+  description = "Map of name values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.name }
 }
 output "spring_cloud_gateway_route_configs_open_api" {
-  description = "List of open_api values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.open_api]
+  description = "Map of open_api values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.open_api }
 }
 output "spring_cloud_gateway_route_configs_predicates" {
-  description = "List of predicates values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.predicates]
+  description = "Map of predicates values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.predicates }
 }
 output "spring_cloud_gateway_route_configs_protocol" {
-  description = "List of protocol values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.protocol]
+  description = "Map of protocol values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.protocol }
 }
 output "spring_cloud_gateway_route_configs_route" {
-  description = "List of route values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.route]
+  description = "Map of route values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.route }
 }
 output "spring_cloud_gateway_route_configs_spring_cloud_app_id" {
-  description = "List of spring_cloud_app_id values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.spring_cloud_app_id]
+  description = "Map of spring_cloud_app_id values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.spring_cloud_app_id }
 }
 output "spring_cloud_gateway_route_configs_spring_cloud_gateway_id" {
-  description = "List of spring_cloud_gateway_id values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.spring_cloud_gateway_id]
+  description = "Map of spring_cloud_gateway_id values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.spring_cloud_gateway_id }
 }
 output "spring_cloud_gateway_route_configs_sso_validation_enabled" {
-  description = "List of sso_validation_enabled values across all spring_cloud_gateway_route_configs"
-  value       = [for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : v.sso_validation_enabled]
+  description = "Map of sso_validation_enabled values across all spring_cloud_gateway_route_configs, keyed the same as var.spring_cloud_gateway_route_configs"
+  value       = { for k, v in azurerm_spring_cloud_gateway_route_config.spring_cloud_gateway_route_configs : k => v.sso_validation_enabled }
 }
 
